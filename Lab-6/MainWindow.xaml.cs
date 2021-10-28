823b26db8196d6781ed7d7e90d516e5f907076b2
аня сергиенко <annsergienko15@gmail.com>
2021-10-28 23:13:04 +0300

    Добавьте файлы проекта.

---
+++ Lab-6/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PairNumber;
+
+namespace Lab_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void resolution(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(firstP1.Text, out int initiFirstP1) && int.TryParse(firstP2.Text, out int initiFirstP2)
+                && int.TryParse(secondP1.Text, out int initiSecondP1) && int.TryParse(secondP2.Text, out int initiSecondP2))
+            {
+                Pair pair1 = new Pair(initiFirstP1, initiSecondP1);
+                Pair pair2 = new Pair(initiFirstP2, initiSecondP2);
+                if (pair1 > pair2 || (pair1 == pair2 && pair1 > initiSecondP2))
+                {
+                    rez.Text = "Перая пара чисел больше, чем вторая";
+                }
+                else rez.Text = "Вторая пара чисел больше, чем первая";
+
+            }
+            else MessageBox.Show("Поля не заполнены или заполнены не верно");
+        }
+
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" ИСП-31, Сергиенко, 4. Создать класс Pair (пара чисел). Создать необходимые методы и свойства. " +
+                " Определить метод сравнения пар: пара p1 больше пары р2, если (first.pl > first.р2)  или (first.pl = first.р2) и (second.pl > second.р2)." +
+                " Создать перегруженные методы  SetParams, для установки параметров объекта.");
+        }
+
+        private void reset(object sender, RoutedEventArgs e)
+        {
+            firstP1.Clear();
+            firstP2.Clear();
+            secondP1.Clear();
+            secondP2.Clear();
+            rez.Clear();
+
+        }
+
+        private void firstP1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rez.Clear();
+        }
+
+        private void secondP1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rez.Clear();
+        }
+
+        private void firstP2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rez.Clear();
+        }
+
+        private void secondP2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rez.Clear();
+        }
+    }
+}
---
+++ Lab-6/Pair.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PairNumber
+{
+    class Pair
+    {
+        public Pair(int initFirst, int initSecond)
+        {
+            _first = initFirst;
+            _second = initSecond;
+        }
+
+        public double _first;
+        public double _second;
+
+        public double First
+        {
+            get
+            {
+                return _first;
+            }
+            set
+            {
+                _first = value;
+            }
+        }
+        public double Second
+        {
+            get
+            {
+                return _second;
+            }
+            set
+            {
+                _second = value;
+            }
+        }
+        public static bool operator >(Pair P1, Pair P2)
+        {
+            return P1.First > P2.First || P1.Second > P2.Second;
+        }
+
+        public static bool operator <(Pair P1, Pair P2)
+        {
+            return P1.First < P2.First || P1.Second < P2.Second;
+        }
+
+        public static bool operator >(Pair P1, int number)
+        {
+            return P1.First > number || P1.Second > number;
+        }
+
+        public static bool operator <(Pair P1, int number)
+        {
+            return P1.First < number || P1.Second < number; ;
+        }
+
+    }
+}
